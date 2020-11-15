9b41d07bab0d262ae91a15a0a3b7794d2d18d4b9
chengche123 <1107640282@qq.com>
2020-11-15 23:02:21 +0800

    修改笔记

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+type anony struct {
+	x, y int
+}
+
+type myType struct {
+	*anony
+	z, w int
+}
+
+type myType2 struct {
+	anony
+	z, w int
+}
+
+func main() {
+	var x myType
+	x.x = 1
+	fmt.Println(x)
 }
