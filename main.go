145d1222d11964b02f1585f5eb94aecea1785aee
chengche123 <1107640282@qq.com>
2020-11-17 14:24:35 +0800

    修改笔记

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 type myT struct {
 	x int
 }
 
 func main() {
-
+	var x interface{} = os.Stdout
+	var y io.Writer = os.Stdout
+	fmt.Println(x == y) // true
 }
