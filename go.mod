7e13a52e024982734e7be20750a487d79821566b
chengche123 <1107640282@qq.com>
2020-11-27 20:29:47 +0800

    修改笔记

--- go.mod
+++ go.mod
@@ -3,10 +3,6 @@ module test
 go 1.13
 
 require (
-	github.com/google/gofuzz v1.2.0
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
-	golang.org/x/text v0.3.4 // indirect
-	gopl.io v0.0.0-20200323155855-65c318dde95e // indirect
-	rsc.io/quote v1.5.2
-	rsc.io/quote/v3 v3.1.0
+	github.com/google/pprof v0.0.0-20201117184057-ae444373da19 // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,134 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
 	"time"
+	"unicode/utf8"
 )
 
 func main() {
-	timer := time.NewTicker(1 * time.Second)
-	for {
-		<-timer.C
-		fmt.Println(time.Now())
-		time.Sleep(3 * time.Second)
-		<-timer.C
-		fmt.Println(time.Now())
-		<-timer.C
-		fmt.Println(time.Now())
+	runtime.SetBlockProfileRate(1)
+
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Panic(err)
 	}
+	defer f.Close()
+	err = pprof.StartCPUProfile(f)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer pprof.StopCPUProfile()
+
+	chs := make(chan struct{}, 2)
+	done := make(chan struct{})
+	tempC := make(chan struct{})
+	go func() {
+		tempC <- struct{}{}
+
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGTERM, os.Kill, os.Interrupt)
+		<-sig
+
+		close(done)
+	}()
+	<-tempC
+
+	go func() {
+		defer func() {
+			chs <- struct{}{}
+		}()
+
+		var result []string
+	loop:
+		for {
+			select {
+			case <-done:
+				break loop
+			default:
+			}
+			time.Sleep(1 * time.Second)
+			result = append(result, reverse1("你好abc123哈哈"))
+		}
+	}()
+
+	go func() {
+		defer func() {
+			chs <- struct{}{}
+		}()
+
+		var result []string
+	loop:
+		for {
+			select {
+			case <-done:
+				break loop
+			default:
+			}
+			time.Sleep(1 * time.Second)
+			result = append(result, reverse2("你好abc123哈哈"))
+		}
+	}()
+
+	// 	var result []string
+	// loop:
+	// 	for {
+	// 		select {
+	// 		case <-done:
+	// 			break loop
+	// 		default:
+	// 		}
+	// 		result = append(result, reverse1("你好abc123哈哈"))
+	// 		time.Sleep(1 * time.Second)
+	// 	}
+
+	<-chs
+	<-chs
+
+	blockFile, err := os.Create("block.prof")
+	if err != nil {
+		panic(err)
+	}
+	err = pprof.Lookup("block").WriteTo(blockFile, 0)
+	if err != nil {
+		panic(err)
+	}
+	blockFile.Close()
+
+	// memf, err := os.Create("mem.prof")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// err = pprof.WriteHeapProfile(memf)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// memf.Close()
+}
+
+func reverse1(s string) string {
+	rs := []rune(s)
+	for p, r := 0, len(rs)-1; p < r; p, r = p+1, r-1 {
+		rs[p], rs[r] = rs[r], rs[p]
+	}
+	return string(rs)
+}
+
+func reverse2(src string) string {
+	n := utf8.RuneCountInString(src)
+	rs := make([]rune, n)
+	for _, v := range src {
+		rs = append([]rune{v}, rs...)
+	}
+	return string(rs[:n])
+	// var newS string
+	// for _, v := range src {
+	// 	newS = string(v) + newS
+	// }
+	// return newS
 }
