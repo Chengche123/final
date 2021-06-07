f5ae409217bee9cda0cbd752aab16f9313db6867
chengche123 <1107640282@qq.com>
2021-06-07 10:25:41 +0800

    更新笔记

--- main.go
+++ main.go
@@ -1,24 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
-
-type fooS struct {
-	f1 uint32
-	f2 byte
-}
-
-type barS struct {
-	f1 uint32
-	f2 byte
-	f3 string
-}
+//go:noinline
+func foo(a int32) {}
 
 func output(int) (int, int, int) // 汇编函数声明
 
+//go:noinline
+func add(a, b int32) (int32, bool) {
+	var c int32 = 999
+	foo(c)
+	return a + b, true
+}
+
 func main() {
 
-	a, b, c := output(987654321)
-	fmt.Println(a, b, c)
+	add(10, 32)
+	output(999)
 }
